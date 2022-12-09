0cacfa5c8584eccfb2e476a31c494b36a8416634
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-12-09 11:23:59 +0530

    Update models.py

--- Adoration/models.py
+++ Adoration/models.py
@@ -37,3 +37,29 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
+    
+    
+
+class Package(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    time = models.CharField(max_length=100, default='1 Day')
+    image = models.ImageField(upload_to='images/')
+    day_descriptions = models.ManyToManyField('PackageDetails', related_name='day_descriptions')
+    inclusions = models.ManyToManyField('inclusion', related_name='inclusions')
+    
+    def __str__(self):
+        return self.name
+
+class PackageDetails(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    def __str__(self):
+        return self.title
+
+class inclusion(models.Model):
+    title = models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
