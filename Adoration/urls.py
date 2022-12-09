85d70096bd30f4324950725cf2c7a9e6024000d7
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-12-09 11:26:23 +0530

    Update urls.py

--- Adoration/urls.py
+++ Adoration/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from Adoration import views
 urlpatterns = [
     path('',views.Main,name = 'Main')
+    
+    path('<str:package>/',views.packageDetails,name = 'PackageDetails')
 ]
-
-# 404 handler
