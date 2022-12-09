2699b3ffe08b0ddc1030391d6ba6b8fdfa68cf4a
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-12-09 11:31:21 +0530

    Update urls.py

--- Adoration/urls.py
+++ Adoration/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from Adoration import views
 urlpatterns = [
-    path('',views.Main,name = 'Main')
+    path('',views.Main,name = 'Main'),
     
     path('<str:package>/',views.packageDetails,name = 'PackageDetails')
 ]
