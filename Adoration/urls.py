125326e067920b4700044c4c976889ab062b7d19
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-12-10 20:13:14 +0530

    robots.txt

--- Adoration/urls.py
+++ Adoration/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from Adoration import views
 from django.contrib.sitemaps.views import sitemap
 from .sitemap import PackageSitemap
+from django.views.generic.base import TemplateView
 urlpatterns = [
     path('',views.Main,name = 'Main'),
     
     path('packages/<int:pk>/',views.packageDetails,name = 'PackageDetails'),
     path('sitemap.xml', sitemap, {'sitemaps': {'article' : PackageSitemap}},
-     name='django.contrib.sitemaps.views.sitemap')
+     name='django.contrib.sitemaps.views.sitemap'),
+    path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain" )),
+
+     
 ]
