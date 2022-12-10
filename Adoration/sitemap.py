d083b278f6885d693d16b60de4e219cd7a3c1f2c
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-12-10 22:31:45 +0530

    frq

--- Adoration/sitemap.py
+++ Adoration/sitemap.py
@@ -2,6 +2,9 @@ from django.contrib.sitemaps import Sitemap
 from .models import Package
   
 class PackageSitemap(Sitemap):
+    changefreq = "weekl"
+    priority = 0.8
+    
     def items(self):
         return Package.objects.all()
         
